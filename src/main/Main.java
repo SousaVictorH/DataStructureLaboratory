8d9385038bc77ca3e050c5e4e9fac0780833626b
SousaVictorH <victor.aquino@ccc.ufcg.edu.br>
2020-10-18 23:10:34 -0300

    corrigindo bugs

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,11 +12,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		KLargestOrderStatisticsImpl kL = new KLargestOrderStatisticsImpl();
+		FloorBinarySearchImpl kL = new FloorBinarySearchImpl();
 		
 		Integer[] lista = {9, 10, 2, 8, 3, 1, 4, 6, 5, 0};
 		
-		System.out.println(Arrays.toString(kL.getKLargest(lista, 11)));
+		System.out.println(kL.floor(lista, -1));
+		System.out.println(kL.floor(lista, 0));
+		System.out.println(kL.floor(lista, 1));
+		System.out.println("");
+		System.out.println(kL.floor(lista, 6));
+		System.out.println(kL.floor(lista, 7));
+		System.out.println(kL.floor(lista, 8));
+		System.out.println("");
+		System.out.println(kL.floor(lista, 10));
+		System.out.println(kL.floor(lista, 11));
 		
 	}
 
