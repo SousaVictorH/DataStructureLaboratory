91a24f9fdd75a8d345bf844e15a6626045933378
SousaVictorH <victor.aquino@ccc.ufcg.edu.br>
2020-10-26 11:26:22 -0300

    adicionando linked lists

--- main/main/Main.java
+++ main/main/Main.java
@@ -14,35 +14,13 @@ public class Main {
 
 	public static void main(String[] args) throws StackOverflowException, StackUnderflowException, QueueUnderflowException, QueueOverflowException {
 		
-		QueueUsingStack q1 = new QueueUsingStack(5);
-		QueueImpl q2 = new QueueImpl(5);
+		QueueUsingStack q1 = new QueueUsingStack(1);
 		
-		System.out.println(q1.head());
-		System.out.println(q2.head());
-		
-		q1.enqueue(1);
-		q2.enqueue(1);
-		
-		q1.enqueue(2);
-		q2.enqueue(2);
-		
-		q1.enqueue(3);
-		q2.enqueue(3);
-		
-		q1.enqueue(4);
-		q2.enqueue(4);
+		System.out.println(q1.isEmpty());
 		
 		q1.enqueue(5);
-		q2.enqueue(5);
 		
-		System.out.println(q1.dequeue());
-		System.out.println(q2.dequeue());
-		
-		System.out.println(q1.dequeue());
-		System.out.println(q2.dequeue());
-		
-		System.out.println(q1.head());
-		System.out.println(q2.head());
+		System.out.println(q1.isFull());
 
 	}
 
